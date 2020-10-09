6e205f6983672e36ed72c1d5e7648649854b040c
divyaniKashyap <71593592+divyaniKashyap@users.noreply.github.com>
2020-10-09 14:18:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,85 @@
+
+var bullet, wall;
+var thickness, speed, weight;
+
+function setup() {
+  createCanvas(1200,400);
+  weight = random(30, 52);
+  speed = random(223,321);
+  thickness = random(22,83);
+  
+  bullet = createSprite(100, 200, 50, 20);
+  wall = createSprite(1000,200,thickness,height/2)
+
+  bullet.shapeColor = "white";
+  bullet.velocityX = speed;
+
+}
+
+function draw() {
+  background("black");
+  
+  if(hasCollided2(bullet,wall)){
+    bullet.velocityX = 0;
+    
+    var damage= (0.5 * weight * speed * speed) / (thickness * thickness * thickness);
+
+    if(damage > 10){
+      wall.shapeColor = "red";
+      textAlign(CENTER);
+      textSize(30);
+      fill("white")
+      text("WALL IS NOT EFFECTIVE AGAINST BULLET", (width/2) - 300 , height/2)
+    }
+
+    if(damage < 10){
+      wall.shapeColor = "green";
+      textAlign(CENTER);
+      textSize(30);
+      fill("white")
+      text("WALL IS EFFECTIVE AGAINST BULLET", (width/2) - 300, height/2)
+    }
+  }
+  drawSprites();
+}
+
+/*
+function hasCollided(){
+
+
+  
+  if(wall.position.x - bullet.position.x < bullet.width/2 + wall.width/2){
+    bullet.velocityX = 0;
+
+    var damage= (0.5 * weight * speed) / (thickness * thickness * thickness);
+
+    if(damage > 10){
+      wall.shapeColor = "red";
+      textAlign(CENTER);
+      textSize(30);
+      fill("white")
+      text("WALL IS NOT EFFECTIVE AGAINST BULLET", (width/2) - 300 , height/2)
+    }
+
+    if(damage < 10){
+      wall.shapeColor = "green";
+      textAlign(CENTER);
+      textSize(30);
+      fill("white")
+      text("WALL IS EFFECTIVE AGAINST BULLET", (width/2) - 300, height/2)
+    }
+  }
+}
+*/
+
+function hasCollided2(bullet,wall){
+  bulletRightEdge = bullet.x + bullet.width/2;
+  wallLeftEdge = wall.x - wall.width/2;
+  
+  if(bulletRightEdge > wallLeftEdge){
+    return true;
+  }else {
+    return false;
+  }
+  
+}
